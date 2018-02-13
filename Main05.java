bb583a3bdc79cf6bbc965b3eb9a5ddf571ffada0
Lime Icing <emil577b@stud.kea.dk>
2018-02-13 20:48:57 +0100

    File structure adjustment

---
+++ Main05.java
@@ -0,0 +1,13 @@
+public class Main05 {
+
+    /*
+    Write a method called sortAndRemoveDuplicates that accepts a list of integers as its parameter and rearranges the
+    list’s elements into sorted ascending order, as well as removing all duplicate values from the list. For example,
+    the list [7, 4, –9, 4, 15, 8, 27, 7, 11, –5, 32, –9, –9] would become [–9, –5, 4, 7, 8, 11, 15, 27, 32] after a call
+    to your method. Use a Set as part of your solution.
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
